2e789f767fc033ba60c9c07a55191b1568d6249e
CJKbarts <bartelsjacobcorn@gmail.com>
2024-03-29 17:42:47 -0500

    Add functionality and style changes
    Add clear button
    Reset display, stored, and operator values
    
    Add delete button and function
    Remove last digit of display values
    
    Fix calculator functionality
    Calculator processes on operation at a time
    
    Change the layout of the buttons on the calculator

--- script.js
+++ script.js
@@ -1,6 +1,81 @@
-let firstOperand;
-let secondOperand;
-let operator;
+
+// Gathering Input
+
+let displayValue = "";
+let storedValue = null;
+let currentOperator = null;
+let operatorEntered = false;
+const displayValueScreen = document.querySelector(".displayValueScreen");
+const storedValueScreen = document.querySelector(".storedValueScreen");
+
+const digitButtons = document.querySelectorAll(".digit");
+digitButtons.forEach(
+    (button) => {
+        button.addEventListener("click",()=> {
+            if (operatorEntered) {
+                storedValue = displayValue;
+                displayValue = button.textContent
+                updateDisplay();
+                operatorEntered = false;
+            }
+
+            else {
+                displayValue += button.textContent;
+                updateDisplay()
+            };
+        })
+    }
+)
+
+function updateDisplay(){
+    displayValueScreen.textContent = displayValue;
+    storedValueScreen.textContent = storedValue;
+}
+
+const operatorButtons = document.querySelectorAll(".operator");
+operatorButtons.forEach(
+    (button) => [
+        button.addEventListener("click", ()=> {
+            if (currentOperator === null) currentOperator = button.textContent;
+
+            if (storedValue !== null) {
+                displayValue = operate(+storedValue, +displayValue, currentOperator);
+                storedValue = null;
+                updateDisplay();
+            }
+
+            currentOperator = button.textContent;   
+            operatorEntered = true;
+        })
+    ]
+)
+
+const clearButton = document.querySelector("#clear");
+clearButton.addEventListener("click", ()=> {
+    storedValue = null;
+    displayValue = '';
+    currentOperator = null;
+    updateDisplay();
+})
+
+const deleteButton = document.querySelector("#delete");
+deleteButton.addEventListener("click", deleteDigit);
+
+function deleteDigit(){
+    displayValue = displayValue.slice(0, -1);
+    updateDisplay();
+}
+
+// Processing Input
+
+const equalsButton = document.querySelector("#equals");
+equalsButton.addEventListener("click", ()=> {
+    displayValue = operate(+storedValue, +displayValue, currentOperator);
+    storedValue = null;
+    updateDisplay();
+})
+
+
 
 function operate(x, y, operation){
     switch (operation){
@@ -33,3 +108,4 @@ function multiply(x, y) {
 function divide(x, y) {
     return (x / y);
 }
+
